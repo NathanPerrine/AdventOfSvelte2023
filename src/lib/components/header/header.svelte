96fa5cfef73dd79c6422111ecf723079ad291123
Nathan <nathan.perrine@gmail.com>
2024-01-17 21:52:43 -0500

    navbar update

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -1,5 +1,14 @@
-<script>
+<script lang="ts">
   import Icon from '@iconify/svelte';
+
+  function toggleTheme() {
+    console.log('toggle')
+  }
+
+  const days = [
+    {'day': 'Day 1', 'href': "/"},
+    {'day': 'Day 2', 'href': "/day2"}
+  ]
 </script>
 <header>
   <div class="navbar bg-base-100">
@@ -9,32 +18,32 @@
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
         </div>
         <button class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-          <li><a href="/"><Icon icon="jam:github" /></a></li>
+          <li><a href="https://github.com/NathanPerrine/AdventOfSvelte2023" target="_blank"><Icon icon="jam:github" width="24" /></a></li>
           <li>
-            <a href="/">Parent</a>
+            <a href="/">Days</a>
             <ul class="p-2">
-              <li><a href="/">Submenu 1</a></li>
-              <li><a href="/">Submenu 2</a></li>
+              {#each days as day}
+                <li><a href="{day.href}">{day.day}</a></li>
+              {/each}
             </ul>
           </li>
-          <li><a href="/">Item 3</a></li>
         </button>
       </div>
       <a href="/" class="btn btn-ghost text-xl">Advent of Svelte 2023</a>
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
-        <li><a href="/"><Icon icon="jam:github" /></a></li>
+        <li><a href="https://github.com/NathanPerrine/AdventOfSvelte2023" target="_blank"><Icon icon="jam:github" width="24" /></a></li>
         <li>
           <details>
-            <summary>Parent</summary>
+            <summary>Days</summary>
             <ul class="p-2">
-              <li><a href="/">Submenu 1</a></li>
-              <li><a href="/">Submenu 2</a></li>
+              {#each days as day}
+                <li><a href="{day.href}">{day.day}</a></li>
+              {/each}
             </ul>
           </details>
         </li>
-        <li><a href="/">Item 3</a></li>
       </ul>
     </div>
     <div class="navbar-end pr-3">
@@ -42,7 +51,7 @@
       <label class="swap swap-rotate">
 
         <!-- this hidden checkbox controls the state -->
-        <input type="checkbox" class="theme-controller" value="sunset" />
+        <input type="checkbox" on:click={toggleTheme} class="theme-controller" value="sunset" />
 
         <!-- sun icon -->
         <svg class="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
