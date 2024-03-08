b400f4114f46b885205d0a7b950605a88144b739
Nathan Perrine <nathan.perrine@gmail.com>
2024-03-08 16:44:40 -0500

    day 5 centered on mobile, day [9] started

---
+++ src/routes/day9/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import Title from '$lib/components/Title.svelte';
+import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+<section class="snow border border-accent h-full">
+  <!-- Title -->
+  <Title day={9} />
+
+
+  <main>
+
+  </main>
+
+
+
+
+
+</section>
+
+<style>
+  .snow {
+    /* height: 90vh; */
+
+  }
+
+  :global(body) {
+    background-color: pink;
+  }
+</style>
